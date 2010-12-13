7af305d1cbfca0d82004036a17f4a95fdb64972f
Daniel Pittman <daniel@rimspace.net>
2010-12-14 10:58:30 +1100

    'puppet cert', for better or worse, tries to manage the confdir and vardir while doing setup, even if ssldir is given elsewhere.  This breaks the test run as a non-root user, which we can unbreak by overriding the default locations.

--- spec/ticket_3961_puppet_ca_should_produce_certs_spec.sh
+++ spec/ticket_3961_puppet_ca_should_produce_certs_spec.sh
@@ -2,6 +2,9 @@
 
 source lib/setup.sh
 
-puppet cert --trace --generate test01.domain.tld --ssldir=/tmp/puppet-ssl-$$ --debug --verbose
+# Using the SSL dir as vardir and confdir is ugly, but workable...
+puppet cert --trace --generate test01.domain.tld \
+    --confdir=/tmp/puppet-ssl-$$ --vardir=/tmp/puppet-ssl-$$ \
+    --ssldir=/tmp/puppet-ssl-$$ --debug --verbose
 
 ls /tmp/puppet-ssl-$$/certs/*
