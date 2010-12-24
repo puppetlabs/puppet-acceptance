5aff37d6b6becb5276387f92f18810c91aeb4177
chromatic <chromatic@wgz.org>
2010-12-23 16:30:41 -0800

    Ported a test to use output_contains.

--- spec/puppet_apply_a_file_should_create_a_file_and_report_the_md5_spec.sh
+++ spec/puppet_apply_a_file_should_create_a_file_and_report_the_md5_spec.sh
@@ -5,12 +5,14 @@ source lib/testlib.sh
 
 rm -f /tmp/hello.world.$$.txt
 
-puppet apply <<PP | grep "defined content as '{md5}098f6bcd4621d373cade4e832627b4f6'"
+rule=$( cat <<PP
 file{ "/tmp/hello.world.$$.txt":
         content => "test",
 }
 PP
+)
 
+output_contains puppet apply $rule "defined content as '{md5}098f6bcd4621d373cade4e832627b4f6'"
 
 # the original test used -f; does -e suffice?
 file_exists /tmp/hello.world.$$.txt
