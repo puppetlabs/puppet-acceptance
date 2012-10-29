874edc3ae52e42fc0f3810f80a1c530049c1e071
Justin Stoller <justin@puppetlabs.com>
2012-10-29 08:50:45 -0700

    further refactoring for package installation

--- lib/puppet_acceptance/dsl/install_utils.rb
+++ lib/puppet_acceptance/dsl/install_utils.rb
@@ -26,34 +26,86 @@ module PuppetAcceptance
         [puppet_depends, puppet, depends_puppet].flatten
       end
 
-      def find_git_repo_versions host, path, repository
-        step "Grab version for #{repository[:name]}"
-        version = {}
-        on host, "cd #{path}/#{repository[:name]} && " +
-                  "git describe || true" do
-          version[repository[:name]] = result.stdout.chomp
+      def create_packages_for_hosts hosts, base_path, repo_names
+        hosts.each do |host|
+          create_packages_for host, base_path, repo_names
         end
-        version
       end
 
-      def install_from_git host, path, repository
+      def create_packages_for host, base_path, repo_names
+        repo_names.each do |name|
+          create_package_for host, "#{base_path}/#{name}"
+        end
+      end
+
+      def create_package_for host, path_to_repository
+        package_cmd = case host['platform']
+                      when /debian/, /ubuntu/
+                        'rake package:deb'
+                      when /el-/
+                        'rake package:rpm'
+                      when /windows/
+                        'sure....'
+                      else
+                        raise ArgumentError,
+                          'I dont know how to create that package'
+                      end
+
+        on host, "cd #{path_to_repository}; #{package_cmd}"
+      end
+
+      def clone_git_repos_for_hosts hosts, path, repositories
+        repo_names = repositories.map {|r| r[:name] }.join(', ')
+        host_names = hosts.map {|h| h.name }.join(', ')
+        step "Cloning #{repo_names} for #{host_names}" do
+
+          hosts.each do |host|
+            clone_git_repos host, path, repositories
+          end
+
+        end
+      end
+
+      def clone_git_repos host, path, repositories
+        repo_names = repositories.map {|r| r[:name] }.join(', ')
+        step "Cloning #{repo_names} for #{host.name}" do
+          repositories.each do |repo|
+            clone_git_repo host, path, repo
+          end
+        end
+      end
+
+      def clone_git_repo host, path, repository
         name   = repository[:name]
         repo   = repository[:path]
         rev    = repository[:rev]
         target = "#{path}/#{name}"
 
-        step "Clone #{repo} if needed"
-        on host, "test -d #{path} || mkdir -p #{path}"
-        on host, "test -d #{target} || git clone #{repo} #{target}"
+        step "Clone #{repo} if needed" do
+          on host, "test -d #{path} || mkdir -p #{path}"
+          on host, "test -d #{target} || git clone #{repo} #{target}"
+        end
 
-        step "Update #{name} and check out revision #{rev}"
         commands = ["cd #{target}",
                     "remote rm origin",
                     "remote add origin #{repo}",
                     "fetch origin",
                     "clean -fdx",
                     "checkout -f #{rev}"]
-        on host, commands.join(" && git ")
+
+        step "Update #{name} and check out revision #{rev}" do
+          on host, commands.join(" && git ")
+        end
+      end
+
+      def install_from_git_repos host, path, names
+        names.each do |name|
+          install_from_git_repo host, path, name
+        end
+      end
+
+      def install_from_git_repo host, path, name
+        target = "#{path}/#{name}"
 
         step "Install #{name} on the system"
         # The solaris ruby IPS package has bindir set to /usr/ruby/1.8/bin.
@@ -69,6 +121,17 @@ module PuppetAcceptance
                     "ruby ./install.rb #{install_opts}; " +
                     "else true; fi"
       end
+
+      def find_git_repo_versions host, path, repository
+        step "Grab version for #{repository[:name]}"
+        version = {}
+        on host, "cd #{path}/#{repository[:name]} && " +
+                  "git describe || true" do
+          version[repository[:name]] = result.stdout.chomp
+        end
+        version
+      end
+
     end
   end
 end
