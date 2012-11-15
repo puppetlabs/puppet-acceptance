b1147a950f628f91377b1ea1eeb5cdf68f82f7c6
Justin Stoller <justin@puppetlabs.com>
2012-11-14 16:57:07 -0800

    Move 'setup/packages' to 'setup/package'

---
+++ setup/package/00_SetupWindowsGems-RemoveAfterIntegratingWindowsPackaging.rb
@@ -0,0 +1,14 @@
+test_name "Setup Windows Gems"
+confine :to, :platform => 'windows'
+
+WINDOWS_GEMS = [
+  'sys-admin -v1.5.6', 'win32console -v1.3.2', 'win32-security -v0.1.4', 'win32-dir -v0.3.7', 'win32-eventlog -v0.5.3',
+  'win32-process -v0.6.5', 'win32-service -v0.7.2', 'win32-taskscheduler -v0.2.2'
+]
+
+hosts.each do |host|
+  WINDOWS_GEMS.each do |gem|
+    step "Installing #{gem}"
+    on host, "cmd /c gem install #{gem} --no-ri --no-rdoc"
+  end
+end
