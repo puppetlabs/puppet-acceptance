62e319d3b03c8c9244cf30e31b3524da77fffb9a
Justin Stoller <justin@puppetlabs.com>
2012-12-10 15:35:55 -0800

    relax sign cert regex

--- setup/package/04_SignCerts.rb
+++ setup/package/04_SignCerts.rb
@@ -1,22 +1,38 @@
 test_name 'Sign Requested Agent Certs' do
 
   agents.each do |agent|
-
     on agent, puppet( 'agent -t' ), :acceptable_exit_codes => [0,1]
 
-    (0..10).each do |i|
-      step "Checking if cert issued for #{agent} (#{i})"
+    signed = false
+    wait = 1
 
-      # puppet cert --list <IP> fails, so list all
-      break if on(
-        master, puppet("cert --list --all")
-      ).stdout =~ /^#{Regexp.escape("+ \"#{agent.name}\"")}/
+    (0..10).each do |i|
+      step "Checking if cert issued for #{agent} (#{i})" do
+        signed_agent_regexp = /^#{Regexp.escape("+ \"#{agent.name}")}/
+
+        on master,
+           puppet("cert --sign --all"),
+           :acceptable_exit_codes => [0,24]
+
+        signing_result = on( master, puppet( 'cert list --all' ) )
+
+        signed = true if signing_result.stdout =~ signed_agent_regexp
+
+        unless signed
+          logger.debug( signing_result.stdout )
+          logger.debug( 'did not match:' )
+          logger.debug( signed_agent_regexp )
+        end
+      end
+
+      break if signed
 
       fail_test("Failed to sign cert for #{agent}") if i == 10
 
-      step "Wait for agent #{agent}: #{i}"
-      sleep 10
-      on master, puppet("cert --sign --all"), :acceptable_exit_codes => [0,24]
+      step "Wait for agent #{agent}: #{i}" do
+        sleep wait
+        wait += i
+      end
     end
   end
 end
