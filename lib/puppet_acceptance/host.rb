75b2dd0c3431a1a399c6b90a2cff9849feb66a89
Scott Schneider <sschneider@puppetlabs.com>
2013-04-05 12:44:05 -0700

    Use the vmname if one is specified

--- lib/puppet_acceptance/host.rb
+++ lib/puppet_acceptance/host.rb
@@ -88,7 +88,7 @@ module PuppetAcceptance
     end
 
     def connection
-      @connection ||= SshConnection.connect( self['ip'] || @name,
+      @connection ||= SshConnection.connect( self['ip'] || @config['HOSTS'][@name]['vmname'] || @name,
                                              self['user'],
                                              self['ssh'] )
     end
