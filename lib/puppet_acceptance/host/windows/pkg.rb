4552ca4775f185bd6633b72407e056a27cb2495f
Alice Nodelman <alice@puppetlabs.com>
2013-07-25 14:56:20 -0700

    rootdir for 64bit = c:\cygwin64, rootdir for 32bit = c:\cygwin

--- lib/puppet_acceptance/host/windows/pkg.rb
+++ lib/puppet_acceptance/host/windows/pkg.rb
@@ -8,16 +8,19 @@ module Windows::Pkg
 
   def install_package name
     cygwin = ""
+    rootdir = ""
     result = exec(PuppetAcceptance::Command.new("wmic os get osarchitecture | grep 32"), :acceptable_exit_codes => (0...127))
     if result.exit_code == 0 #32 bit version
+      rootdir = "c:\\\\cygwin"
       cygwin = "setup-x86.exe"
     else  #64 bit version
+      rootdir = "c:\\\\cygwin64"
       cygwin = "setup-x86_64.exe"
     end
     if not check_for_package(cygwin)
       execute("curl --retry 5 http://cygwin.com/#{cygwin} -o /cygdrive/c/Windows/System32/#{cygwin}")
     end
-    execute("#{cygwin} -q -n -N -d -R c:\\\\cygwin -s http://cygwin.osuosl.org -P #{name}") 
+    execute("#{cygwin} -q -n -N -d -R #{rootdir} -s http://cygwin.osuosl.org -P #{name}") 
   end
 
 end
